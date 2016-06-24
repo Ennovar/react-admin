4d6685a79891646ffdea2f8db9b2286e4211d542
jerekshoe <jerek@passfail.com>
2016-06-24 11:39:53 -0500

    fixed reducer

--- dist/index.js
+++ dist/index.js
@@ -3,21 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.reducer = exports.routes = undefined;
-
-var _routes = require('./routes');
-
-var _routes2 = _interopRequireDefault(_routes);
+exports.reducer = undefined;
 
 var _reducers = require('./reducers');
-
-var _reducers2 = _interopRequireDefault(_reducers);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // export default const App = () => (
 //   <Router history={browserHistory} routes={routes} />
 // );
-// Libary imports
-exports.routes = _routes2.default;
-exports.reducer = _reducers2.default;
+exports.reducer = _reducers.reducer; // Libary imports
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 // Libary imports
-import routes from './routes';
-import reducer from './reducers';
+import { reducer } from './reducers';
 
 // export default const App = () => (
 //   <Router history={browserHistory} routes={routes} />
 // );
 export {
-  routes,
   reducer,
 };
