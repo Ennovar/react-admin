83872f4a9faec46b2a10cad7fadf3e19f26c8e37
Austin Crane <amcrane1@wichita.edu>
2016-06-24 10:33:38 -0500

    added example project

---
+++ webpack.conf.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'inline-source-map', //just do inline source maps instead of the default
+  entry: ['bootstrap-loader'],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
+      { test: /\.png$/, loader: 'url-loader?limit=100000' },
+      { test: /\.jpg$/, loader: 'file-loader' },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file',
+      },
+
+      // // Use one of these to serve jQuery for Bootstrap scripts:
+      //
+      // // Bootstrap 4
+      // { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' },
+      //
+      // Bootstrap 3
+      { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
+    ],
+  },
+}
