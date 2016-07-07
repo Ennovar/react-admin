bf099c931a956580ec9572e0e75eceece9ee322b
jerekshoe <jereks88@gmail.com>
2016-07-07 10:55:11 -0500

    Started working on tests

--- src/test/components/TextField-test.js
+++ src/test/components/TextField-test.js
@@ -4,13 +4,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-addons-test-utils';
 
+const textFieldProps = {
+  mode: 'view',
+  title: 'Title',
+  value: 'value',
+};
+
 describe('TextField', () => {
   it('should render', () => {
-    // ReactDOM.render(
-    //   <TextField />,
-    //   document.body
-    // );
-    const root = ReactTestUtils.renderIntoDocument(<TextField title={'here'} value={'happy birthday'} mode={'view'} />);
-    expect(root).toExist();
+    const textField = ReactTestUtils.renderIntoDocument(<TextField {...textFieldProps} />);
+    expect(textField).toExist();
+    expect(textField.state.mode).toEqual('view');
+    expect(textField.state.value).toEqual('value');
+
+    const h4 = ReactTestUtils.findRenderedDOMComponentWithTag(
+      textField, 'h4'
+    );
+
+    const p = ReactTestUtils.findRenderedDOMComponentWithTag(
+      textField, 'p'
+    );
+
+    expect(ReactDOM.findDOMNode(h4).textContent).toEqual('Title');
+    expect(ReactDOM.findDOMNode(p).textContent).toEqual('value');
+  });
+
+  it('onEditClick', () => {
+    const textField = ReactTestUtils.renderIntoDocument(<TextField {...textFieldProps} />);
+    expect(textField).toExist();
+
+    const fa = ReactTestUtils.findRenderedDOMComponentWithTag(
+      textField, 'i'
+    );
+
+    ReactTestUtils.Simulate.click(fa);
+    expect(textField.state.mode).toEqual('edit');
+  });
+
+  it('changeText', () => {
+    const textField = ReactTestUtils.renderIntoDocument(<TextField {...textFieldProps} />);
+    expect(textField).toExist();
+
+    const fa = ReactTestUtils.findRenderedDOMComponentWithTag(
+      textField, 'i'
+    );
+
+    ReactTestUtils.Simulate.click(fa);
+    expect(textField.state.mode).toEqual('edit');
+
+    const input = ReactTestUtils.findRenderedDOMComponentWithTag(
+      textField, 'input'
+    );
+
+    ReactTestUtils.Simulate.change(input, { target: { value: 'new value' } });
+    expect(textField.state.value).toEqual('new value');
   });
 });
